650ffe4aea674885ae1afb43a6e02c7b34f08051
henry <mr_henrywang@163.com>
2022-01-04 14:17:46 +0800

    user regist interface (1.verify username is exist; 2.regist user;)

--- foodie-api/src/main/java/com/foodie/backend/Application.java
+++ foodie-api/src/main/java/com/foodie/backend/Application.java
@@ -13,7 +13,7 @@ import tk.mybatis.spring.annotation.MapperScan;
 //扫描 mybatis 通用 mapper 所在的包
 @MapperScan(basePackages = "com.foodie.backend.mapper")
 //扫描所有包以及相关组件包
-//@ComponentScan(basePackages = {"com.foodie.backend", "org.n3r.idworker"})   //用来扫描idworker，创建全局唯一性id
+@ComponentScan(basePackages = {"com.foodie.backend", "org.n3r.idworker"})   //用来扫描idworker，创建全局唯一性id
 //@EnableTransactionManagement 可加可不加，原因是在@SpringBootApplication中已经自动装配了
 public class Application {
 
