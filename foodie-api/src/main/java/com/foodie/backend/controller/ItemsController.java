0f82ea84fd0b9e3f61e157b768d2d0e8c1a2e27a
henry <mr_henrywang@163.com>
2022-01-09 12:02:03 +0800

    add get item info interface

--- foodie-api/src/main/java/com/foodie/backend/controller/ItemsController.java
+++ foodie-api/src/main/java/com/foodie/backend/controller/ItemsController.java
@@ -1,52 +1,56 @@
-//package com.foodie.backend.controller;
-//
-//import com.foodie.backend.pojo.vo.CommentLevelCountsVO;
-//import com.foodie.backend.pojo.vo.ItemInfoVO;
-//import com.foodie.backend.pojo.vo.ShopcartVO;
-//import service.ItemService;
-//import com.foodie.backend.utils.IMOOCJSONResult;
-//import com.foodie.backend.utils.PagedGridResult;
-//import io.swagger.annotations.Api;
-//import io.swagger.annotations.ApiOperation;
-//import io.swagger.annotations.ApiParam;
-//import org.apache.commons.lang3.StringUtils;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//
-//@Api(value = "商品接口", tags = {"商品信息展示的相关接口"})
-//@RestController
-//@RequestMapping("items")
-//public class ItemsController extends BaseController {
-//
-//    @Autowired
-//    private ItemService itemService;
-//
-//    @ApiOperation(value = "查询商品详情", notes = "查询商品详情", httpMethod = "GET")
-//    @GetMapping("/info/{itemId}")
-//    public IMOOCJSONResult info(
-//            @ApiParam(name = "itemId", value = "商品id", required = true)
-//            @PathVariable String itemId) {
-//
-//        if (StringUtils.isBlank(itemId)) {
-//            return IMOOCJSONResult.errorMsg(null);
-//        }
-//
-//        Items item = itemService.queryItemById(itemId);
-//        List<ItemsImg> itemImgList = itemService.queryItemImgList(itemId);
-//        List<ItemsSpec> itemsSpecList = itemService.queryItemSpecList(itemId);
-//        ItemsParam itemsParam = itemService.queryItemParam(itemId);
-//
-//        ItemInfoVO itemInfoVO = new ItemInfoVO();
-//        itemInfoVO.setItem(item);
-//        itemInfoVO.setItemImgList(itemImgList);
-//        itemInfoVO.setItemSpecList(itemsSpecList);
-//        itemInfoVO.setItemParams(itemsParam);
-//
-//        return IMOOCJSONResult.ok(itemInfoVO);
-//    }
-//
+package com.foodie.backend.controller;
+
+import com.foodie.backend.pojo.Items;
+import com.foodie.backend.pojo.ItemsImg;
+import com.foodie.backend.pojo.ItemsParam;
+import com.foodie.backend.pojo.ItemsSpec;
+import com.foodie.backend.pojo.vo.CommentLevelCountsVO;
+import com.foodie.backend.pojo.vo.ItemInfoVO;
+import com.foodie.backend.pojo.vo.ShopcartVO;
+import com.foodie.backend.service.ItemService;
+import com.foodie.backend.utils.IMOOCJSONResult;
+import com.foodie.backend.utils.PagedGridResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Api(value = "商品接口", tags = {"商品信息展示的相关接口"})
+@RestController
+@RequestMapping("items")
+public class ItemsController extends BaseController {
+
+    @Autowired
+    private ItemService itemService;
+
+    @ApiOperation(value = "查询商品详情", notes = "查询商品详情", httpMethod = "GET")
+    @GetMapping("/info/{itemId}")
+    public IMOOCJSONResult info(
+            @ApiParam(name = "itemId", value = "商品id", required = true)
+            @PathVariable String itemId) {
+
+        if (StringUtils.isBlank(itemId)) {
+            return IMOOCJSONResult.errorMsg(null);
+        }
+
+        Items item = itemService.queryItemById(itemId);
+        List<ItemsImg> itemImgList = itemService.queryItemImgList(itemId);
+        List<ItemsSpec> itemsSpecList = itemService.queryItemSpecList(itemId);
+        ItemsParam itemsParam = itemService.queryItemParam(itemId);
+
+        ItemInfoVO itemInfoVO = new ItemInfoVO();
+        itemInfoVO.setItem(item);
+        itemInfoVO.setItemImgList(itemImgList);
+        itemInfoVO.setItemSpecList(itemsSpecList);
+        itemInfoVO.setItemParams(itemsParam);
+
+        return IMOOCJSONResult.ok(itemInfoVO);
+    }
+
 //    @ApiOperation(value = "查询商品评价等级", notes = "查询商品评价等级", httpMethod = "GET")
 //    @GetMapping("/commentLevel")
 //    public IMOOCJSONResult commentLevel(
@@ -173,4 +177,4 @@
 //
 //        return IMOOCJSONResult.ok(list);
 //    }
-//}
+}
