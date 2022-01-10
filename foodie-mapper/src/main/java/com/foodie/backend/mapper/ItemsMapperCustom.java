a7f4dd5396f5368073c271ef4de453c32476723c
henry <mr_henrywang@163.com>
2022-01-10 12:29:37 +0800

    add get item comment level & comments interface

--- foodie-mapper/src/main/java/com/foodie/backend/mapper/ItemsMapperCustom.java
+++ foodie-mapper/src/main/java/com/foodie/backend/mapper/ItemsMapperCustom.java
@@ -1,24 +1,27 @@
-//package com.foodie.backend.mapper;
-//
-//
-//import com.foodie.backend.pojo.vo.ItemCommentVO;
-//import com.foodie.backend.pojo.vo.SearchItemsVO;
-//import com.foodie.backend.pojo.vo.ShopcartVO;
-//import org.apache.ibatis.annotations.Param;
-//
-//import java.util.List;
-//import java.util.Map;
-//
-//public interface ItemsMapperCustom {
-//
-//    public List<ItemCommentVO> queryItemComments(@Param("paramsMap") Map<String, Object> map);
-//
+package com.foodie.backend.mapper;
+
+import com.foodie.backend.pojo.vo.ItemCommentVO;
+import com.foodie.backend.pojo.vo.SearchItemsVO;
+import com.foodie.backend.pojo.vo.ShopcartVO;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+
+@Mapper
+@Component
+public interface ItemsMapperCustom {
+
+    public List<ItemCommentVO> queryItemComments(@Param("paramsMap") Map<String, Object> map);
+
 //    public List<SearchItemsVO> searchItems(@Param("paramsMap") Map<String, Object> map);
 //
 //    public List<SearchItemsVO> searchItemsByThirdCat(@Param("paramsMap") Map<String, Object> map);
 //
 //    public List<ShopcartVO> queryItemsBySpecIds(@Param("paramsList") List specIdsList);
-//
-////    public int decreaseItemSpecStock(@Param("specId") String specId,
-////                                     @Param("pendingCounts") int pendingCounts);
-//}
+
+//    public int decreaseItemSpecStock(@Param("specId") String specId,
+//                                     @Param("pendingCounts") int pendingCounts);
+}
